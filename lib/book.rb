878ccbda1bb8c43f3e17842d22cdfb2188e2ebac
Roger Mendez <roger61087@gmail.com>
2021-12-18 15:53:30 -0500

    commit tree 7af94e5f1af5f73c4006dfbc1391a1a8db2fe50b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+require "pry"
+
+class Book
+
+    attr_accessor :author, :title, :page_count, :genre
+    
+     def initialize(title)
+        @title = title
+     end
+
+
+    def author_name(author)
+       @author = author
+        
+    end
+
+    def pages(page_count)
+        @page_count = page_count
+        
+    end
+
+    def genre_type(genre)
+        @genre = genre
+        
+    end
+    
+    def turn_page
+
+       puts  "Flipping the page...wow, you read fast!"
+    end
+    
+    
+    
+    
+    
+end
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,39 @@
 # Make your shoe class here!
+
+require "pry"
+
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition
+
+
+def initialize(brand)
+    @brand = brand
+end
+
+def shoe_color(color)
+    @color = color
+    
+end
+def shoe_size(size)
+    @size = size
+    
+end
+def shoe_material(material)
+    @material = material    
+end
+def shoe_condition(condition)
+    @condition = condition
+    
+end
+def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+end
+
+
+
+
+
+
+
+end
